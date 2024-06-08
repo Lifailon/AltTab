a5021960dc99011e2329fcfd244cdd140653a966
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2024-06-09 02:18:08 +0300

    Update AltTab.sh

--- AltTab.sh
+++ AltTab.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# Не сохранять историю команд
 HISTFILE=
-while true; do
-    # Нажимает и удерживает клавишу Alt, следом клавишу Tab (имитируя переключение окон) и keyup отпускает клавишу Alt
+function invoke-alttab {
     xdotool keydown alt key Tab keyup alt
-    # Пауза между нажатиями в диапазоне от 30 до 179 секунд
     sleep $((RANDOM % 150 + 30))
-done
-# Очистить историю команд
-# history -c && rm ~/.bash_history
+    invoke-alttab
+}
+invoke-alttab
